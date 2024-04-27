836c2291a2fa168e1d545ce5f7cabb10a71b999d
sanatmamyrbek <mamyrbeksanat282@gmail.com>
2024-04-27 08:08:53 +0500

    123

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,18 +6,18 @@ public class Main {
     public static void main(String[] args) {
         MyArrayList myArrayList = new MyArrayList<>();
 
-        myArrayList.add(5);
-        myArrayList.add(4);
         myArrayList.add(3);
         myArrayList.add(2);
         myArrayList.add(1);
+        myArrayList.add(5);
+        myArrayList.add(4);
 
         System.out.println("List before sorting");
         for (int i = 0; i < myArrayList.Size(); i++) {
             System.out.println(myArrayList.get(i));
         }
         myArrayList.sort();
-        System.out.println("List after sort");
+        System.out.println("List after sorting");
         for (int i = 0; i < myArrayList.Size(); i++) {
             System.out.println(myArrayList.get(i));
         }
@@ -31,15 +31,17 @@ public class Main {
         myArrayList.remove(2);
         System.out.println("Vaule of index 2 after remove: " + myArrayList.get(2));
         System.out.println("Size of an list: " + myArrayList.Size());
-
+/**
+ *
+ */
 
         MyLinkedList myLinkedList = new MyLinkedList<>();
 
-        myLinkedList.add(5);
-        myLinkedList.add(4);
-        myLinkedList.add(3);
-        myLinkedList.add(2);
         myLinkedList.add(1);
+        myLinkedList.add(2);
+        myLinkedList.add(3);
+        myLinkedList.add(4);
+        myLinkedList.add(5);
         new Stack<>();
 
         System.out.println("List before sorting");
@@ -47,7 +49,7 @@ public class Main {
             System.out.println(myLinkedList.get(i));
         }
         myLinkedList.sort();
-        System.out.println("List after sort");
+        System.out.println("List after sorting");
         for (int i = 0; i < myLinkedList.Size(); i++) {
             System.out.println(myLinkedList.get(i));
         }
@@ -61,14 +63,16 @@ public class Main {
         myLinkedList.remove(2);
         System.out.println("Vaule of index 2 after remove: " + myLinkedList.get(2));
         System.out.println("Size of an list: " + myLinkedList.Size());
-
+/**
+ *
+ */
         Classes.MyStack myStack = new Classes.MyStack<>();
         System.out.println("Check stack emptiness: " + myStack.isEmpty());
-        myStack.push(5);
-        myStack.push(4);
-        myStack.push(3);
-        myStack.push(2);
         myStack.push(1);
+        myStack.push(2);
+        myStack.push(3);
+        myStack.push(4);
+        myStack.push(5);
 
         System.out.println("Peeked element: " + myStack.peek());
         System.out.println("Popped element: " + myStack.pop());
@@ -76,7 +80,9 @@ public class Main {
         System.out.println("Popped element: " + myStack.pop());
         System.out.println("Check stack emptiness: " + myStack.isEmpty());
         System.out.println("Check stack size: " + myStack.size());
-
+/**
+ *
+ */
         MyQueue myQueue = new MyQueue();
         System.out.println("Check queue emptiness: " + myQueue.isEmpty());
         myQueue.enqueue(5);
@@ -91,16 +97,18 @@ public class Main {
         System.out.println("Dequeued element: " + myQueue.dequeue());
         System.out.println("Check queue emptiness: " + myQueue.isEmpty());
         System.out.println("Check queue size: " + myQueue.size());
-
+/**
+ *
+ */
         MyMinHeap myMinHeap = new MyMinHeap<>();
         System.out.println("Is the heap empty? " + myMinHeap.isEmpty());
 
         // Insert elements
-        myMinHeap.insert(5);
-        myMinHeap.insert(10);
         myMinHeap.insert(3);
-        myMinHeap.insert(7);
+        myMinHeap.insert(10);
         myMinHeap.insert(1);
+        myMinHeap.insert(6);
+        myMinHeap.insert(9);
 
         System.out.println("Minimum element: " + myMinHeap.getMin());
         System.out.println("Extracted element is: " + myMinHeap.extractMin());
